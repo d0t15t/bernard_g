2790ebf5f19dfdfe810c4329df6e782807cf583a
Isaac Bigsby Trogdon <mr@dyss.net>
2018-02-26 11:44:18 +0100

    Initial commit.

---
+++ www/sites/all/themes/bernardg_theme/js/scripts.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($){
+
+$('.node-type-article .info-group').prepend('<div id="custom-col-header"><div id="pane1"></div><div id="pane2"></div></div>');
+$('.node-type-article .group-info').prepend('<div id="custom-col-header"><div id="pane1"></div><div id="pane2"></div></div>');
+$('.node-blog .info-group').prepend('<div id="custom-col-header"><div id="pane1"></div><div id="pane2"></div></div>');
+$('.node-blog .group-info').prepend('<div id="custom-col-header"><div id="pane1"></div><div id="pane2"></div></div>');
+
+});
